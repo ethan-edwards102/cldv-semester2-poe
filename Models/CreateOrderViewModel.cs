60d5f7bbc631debd2f75e49f706386a5dc5579bc
Ethan Edwards <st10442420@gmail.com>
2025-08-20 14:00:35 +0200

    Order Edit View Adjusments

---
+++ Models/CreateOrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Azure;
+
+namespace ABCRetails.Models;
+
+public class OrderViewModel
+{
+    public string CustomerRowKey { get; set; }
+    public string ProductRowKey { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ Models/EditOrderViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Azure;
+
+namespace ABCRetails.Models;
+
+public class EditOrderViewModel
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+
+    // Azure Table fields
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+
+    // When editing, product and customer details
+    // can either be kept the same or changed
+    public string? ProductRowKey { get; set; }
+    public string? ProductName { get; set; }
+    public string? PhotoURL { get; set; }
+
+    public string? CustomerRowKey { get; set; }
+    public string? CustomerName { get; set; }
+    public string? DeliveryAddress { get; set; }
+    
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public EditOrderViewModel(Order order)
+    {
+        PartitionKey = order.PartitionKey;
+        RowKey = order.RowKey;
+        Timestamp = order.Timestamp;
+        ETag = order.ETag;
+        ProductName = order.ProductName;
+        PhotoURL = order.PhotoURL;
+        CustomerName = order.CustomerName;
+        DeliveryAddress = order.DeliveryAddress;
+        Quantity = order.Quantity;
+        TotalPrice = order.TotalPrice;
+    }
+}
